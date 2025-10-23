cb7e0ed3d4e7c38f2d223d259b68c5882b06c5d6
Lesley Hogg <lesleyhogg@gmail.com>
2025-10-23 13:20:11 -0400

    custom typewriter effect hook

---
+++ src/hooks/useTypewriter.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function useTypewriter(text: string, speed: number = 50) {
+  const [displayText, setDisplayText] = useState('')
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    if (currentIndex < text.length) {
+      const timeout = setTimeout(() => {
+        setDisplayText(prev => prev + text[currentIndex])
+        setCurrentIndex(prev => prev + 1)
+      }, speed)
+
+      return () => clearTimeout(timeout)
+    }
+  }, [currentIndex, text, speed])
+
+  return displayText
+}
