d32bfb6ed7046caf97dd6f9098ffc3b1fd57bb05
Lesley Hogg <lesleyhogg@gmail.com>
2025-10-24 13:54:13 -0400

    remove unnecessary context

--- src/app/(dashboard)/_components/NavButton.tsx
+++ src/app/(dashboard)/_components/NavButton.tsx
@@ -8,7 +8,7 @@ export interface NavButtonProps {
 export default function NavButton({handleOnClick, label, isCurrentPathname}: NavButtonProps) {
  
   return (
-    <button onClick={handleOnClick} className={`w-20 h-20 rounded-lg border-2 w-full text-center shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,0.8)] font-semibold font-[family-name:var(--font-vt323)] ${isCurrentPathname ? 'text-4xl' : 'text-xl'}`}>
+    <button onClick={handleOnClick} className={`h-20 rounded-lg border-2 w-full text-center shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,0.8)] font-semibold font-[family-name:var(--font-vt323)] ${isCurrentPathname ? 'text-4xl' : 'text-xl'}`}>
       {label}
     </button>
   )
